c140ecf55e3300058c2bf94b2cc931c453d31624
Vincent Kariuki <vincentkariuki07@gmail.com>
2025-12-12 00:48:46 +0300

    chore: Adda Rector, Larastan and Pint to dev dependencies for automated code quality and formatting, and their rules.

---
+++ rector.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withComposerBased(laravel: true)
+    ->withPhpVersion(PhpVersion::PHP_84)
+    ->withCache(
+        cacheDirectory: '/tmp/rector',
+        cacheClass: FileCacheStorage::class,
+    )
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/bootstrap/app.php',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    ->withSkip([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+    )
+    ->withParallel();
