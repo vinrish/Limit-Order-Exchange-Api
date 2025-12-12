25f803a5b0452bcd29c94074b0fc3b1152ea6da7
Vincent Kariuki <vincentkariuki07@gmail.com>
2025-12-12 07:19:24 +0300

    test: Updates Model unit tests

--- tests/Unit/Models/AssetTest.php
+++ tests/Unit/Models/AssetTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Models\Asset;
+use App\Models\User;
 use Carbon\CarbonInterface;
 
 it('returns an array', function (): void {
@@ -34,4 +35,14 @@ it('applies casts correctly', function (): void {
     expect($asset->id)->toBeString()
         ->and($asset->created_at)->toBeInstanceOf(CarbonInterface::class)
         ->and($asset->updated_at)->toBeInstanceOf(CarbonInterface::class);
+});
+
+it('belongs to a user', function (): void {
+    $user = User::factory()->create();
+    $asset = Asset::factory()->create([
+        'user_id' => $user->id,
+    ])->fresh();
+
+    expect($asset->user->id)->toBe($user->id)
+        ->and($asset->user)->toBeInstanceOf(User::class);
 });
--- tests/Unit/Models/OrderTest.php
+++ tests/Unit/Models/OrderTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Models\Order;
+use App\Models\User;
 
 it('returns an array', function (): void {
     $order = Order::factory()->create()->fresh();
@@ -18,4 +19,14 @@ it('returns an array', function (): void {
         'created_at',
         'updated_at',
     ]);
+});
+
+it('belongs to a user', function (): void {
+    $user = User::factory()->create();
+    $order = Order::factory()->create([
+        'user_id' => $user->id,
+    ])->fresh();
+
+    expect($order->user->id)->toBe($user->id);
+    expect($order->user)->toBeInstanceOf(User::class);
 });
